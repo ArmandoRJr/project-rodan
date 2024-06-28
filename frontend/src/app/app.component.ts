1620826b1c91f6485591f08fe07aea2147d1a752
ArmandoRJr <59779955+ArmandoRJr@users.noreply.github.com>
2024-06-28 16:31:20 -0400

    task: added basic setup + CI/CD

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,31 @@
+// src/app/app.component.ts
+import { Component, OnInit } from "@angular/core";
+import { io, Socket } from "socket.io-client";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  private socket: Socket;
+  message: string = "";
+  messages: string[] = [];
+
+  constructor() {
+    this.socket = io("https://api.rodan.armandorojas.me");
+  }
+
+  ngOnInit(): void {
+    this.socket.on("message", (msg: string) => {
+      this.messages.push(msg);
+    });
+  }
+
+  sendMessage(): void {
+    if (this.message.trim()) {
+      this.socket.emit("message", this.message);
+      this.message = "";
+    }
+  }
+}
