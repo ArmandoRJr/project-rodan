1f29fc005452b4984a6c36ecffc5008db2b04159
ArmandoRJr <59779955+ArmandoRJr@users.noreply.github.com>
2024-07-05 09:15:51 -0400

    task: added basic game logic

---
+++ frontend/src/app/components/game-test/game-test.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { io } from 'socket.io-client';
+
+@Component({
+  selector: 'app-game-test',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game-test.component.html',
+  styleUrl: './game-test.component.css',
+})
+export class GameTestComponent implements OnInit {
+  private socket;
+  roomData: any;
+  roundData: any;
+  scores: any = {};
+  userId: string;
+  isSpectator: boolean = false;
+  isPlayer: boolean = false;
+
+  imageForm = new FormGroup({
+    image: new FormControl(''),
+  });
+
+  constructor() {
+    this.socket = io('http://localhost:3000');
+    this.userId = this.socket.id ?? '';
+  }
+
+  ngOnInit() {
+    this.socket.on('playerJoined', (data) => {
+      this.userId = data.playerId;
+      this.isPlayer = true;
+    });
+
+    this.socket.on('spectatorJoined', (data) => {
+      this.userId = data.spectatorId;
+      this.isSpectator = true;
+    });
+
+    this.socket.on('roomData', (data) => {
+      this.roomData = data;
+    });
+
+    this.socket.on('newRound', (data) => {
+      this.roundData = data;
+    });
+
+    this.socket.on('score', (data) => {
+      this.scores[data.userId] = this.scores[data.userId] || [];
+      this.scores[data.userId].push(data.score);
+    });
+
+    this.socket.on('roundEnded', (scores) => {
+      this.scores = scores;
+    });
+
+    this.socket.on('gameOver', (scores) => {
+      this.scores = scores;
+      alert('Game Over!');
+    });
+
+    this.socket.emit('joinRoom', 'someRoomId');
+  }
+
+  submitPicture(picture: Event) {
+    const input = picture.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.socket.emit('submitPicture', 'someRoomId', file);
+    }
+    console.log(picture);
+    // if (this.isPlayer) {
+    //   this.socket.emit('submitPicture', 'someRoomId', picture);
+    // } else {
+    //   alert('Spectators cannot submit pictures.');
+    // }
+  }
+}
